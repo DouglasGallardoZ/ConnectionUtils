dc7d9b2efac7db510a3edd79d38d2be26ba15f2b
dgallardo <douglasgallardoz11@gmail.com>
2021-11-28 13:21:07 -0500

    Se genera clase ConnectionUtils para coneccion a base de datos MySQL

---
+++ src/main/java/com/dgz/util/ConnectionUtils.java
@@ -0,0 +1,36 @@
+package com.dgz.util;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+
+    private static final Logger LOGGER = Logger.getLogger(ConnectionUtils.class);
+    private final static String USER = "root";
+    private final static String PASSWORD = "rootpw";
+    private final static String PORT = "3306";
+
+    private static String jurl = "jdbc:mysql://localhost:"+PORT+"/prueba";
+
+
+
+    public static Connection getConnection(){
+        BasicConfigurator.configure();
+        LOGGER.info("Iniciando conexion a Base de Datos");
+        Connection con = null;
+
+        try {
+            con = DriverManager.getConnection(jurl, USER, PASSWORD);
+            LOGGER.info("Conexion exitosa");
+        } catch (SQLException e) {
+            LOGGER.info("Error en conexion");
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+}
